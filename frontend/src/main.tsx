922cfa6e87d89ba28dfb263bce1707e164e8cf08
pedromellodev <pedromello19122006@gmail.com>
2024-10-07 17:23:23 -0300

    Merge

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,8 @@
-// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./app.tsx";
+import { App } from "./app";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { AuthProvider } from "./cli/AuthContext.tsx";
+import { AuthProvider } from "./cli/AuthContext";
 
 const queryClient = new QueryClient();
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
