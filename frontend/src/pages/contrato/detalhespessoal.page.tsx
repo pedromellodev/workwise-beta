b5735f78bbb54162805f7c7d2d04c8cae6497cf8
Nath Gonçalves <neganath2006@gmail.com>
2024-11-26 11:54:26 -0300

    receba ph

--- frontend/src/pages/contrato/detalhespessoal.page.tsx
+++ frontend/src/pages/contrato/detalhespessoal.page.tsx
@@ -68,7 +68,7 @@ export default function Detalhes() {
     switch (selectedOption) {
       case "personal":
         return (
-          <div>
+          <div className="w-full">
           <h2 className="bg-purple-400 text-white text-center py-2 rounded-md mb-6">Dados Pessoais</h2>
           <div className="grid grid-cols-[200px,1fr] gap-4">
             {personalInfo.map((item) => (
@@ -167,15 +167,16 @@ export default function Detalhes() {
   };
 
   return (
-    <div className="h-screen bg-gradient-to-br from-purple-500 via-purple-400 to-blue-500 flex flex-col overflow-hidden">
+    <div className="h-screen bg-gradient-to-br from-purple-500 via-purple-400 to-blue-500 flex flex-col">
       {/* Header */}
       <div className="w-full flex justify-between items-center bg-purple-300">
         <img src={workwise_logo} alt="WorkWise Logo" className="h-12" />
       </div>
       
       {/* Main Content */}
-      <main className="container mx-auto p-3">
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden mx-[42px] my-[48px]">
+      <main className="container mx-auto p-3 overflow-y-auto h-screen scrollbar-hidden"
+      style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden mx-[42px] my-[px]">
           {/* Header com fundo roxo */}
           <header className="bg-purple-500 text-white p-4 rounded-t-lg mb-12">
             <div className="p-4 flex justify-between">
@@ -196,9 +197,9 @@ export default function Detalhes() {
           </header>
           
           {/* Grid Container */}
-          <div className="grid grid-cols-[373px,1256px] gap-4 mt-6">
+          <div className="grid grid-cols-[373px,750px] max-w-full max-h-full gap-4 mt-6">
             {/* Sidebar */}
-            <div className="w-[277px] h-[430px] border border-gray-300 rounded-lg overflow-hidden p-6 ml-[60px]">
+            <div className="max-w-full max-h-full border border-gray-300 rounded-lg overflow-hidden p-6 ml-[60px]">
               <aside>
                 <div className="bg-purple-400 text-white font-medium text-center py-4 rounded-md mb-4">
                   Opções
@@ -219,7 +220,7 @@ export default function Detalhes() {
 
             {/* Content Area */}
             <div 
-              className="bg-white w-[1000px] h-[600px] border border-gray-300 rounded-md p-6 mb-[50px] overflow-y-auto" 
+              className="bg-white w-full max-h-[600px] h-full border border-gray-300 rounded-md p-6 mb-[50px] overflow-y-auto" 
               style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}
             >
               {renderContent()}
