55a3858a17812be7e866d0b716712c016190d862
Matheus <matheus.jesus6505@gmail.com>
2024-11-27 02:54:17 -0300

    fix error(recsenha)

--- frontend/src/routes/admin.route.tsx
+++ frontend/src/routes/admin.route.tsx
@@ -4,6 +4,7 @@ import { Home } from "../pages/home.page";
 import Contratos from "../pages/contrato/contratos.page";
 import { Detalhes } from "../pages/contrato/detalhes.page";
 import { Avaliacoes } from "../pages/contrato/avaliacoes";
+import { Recsenha } from "../pages/recsenha.page";
 
 export function AdminRouter() {
 	return (
